2979007b0479d0b5e4b7a4b915e9e055656da0de
brianskiies <brian.he3@gmail.com>
2020-12-11 21:21:16 +1100

    fixed bug in level1

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -1,11 +1,3 @@
-# require_relative '../punishments'
-
-# describe 'punishment' do
-#     it 'should select a random punishment from the array' do
-#         expect(Punishments.random_punishment(['do 10 pushups']).to eq("do 10 pushups"))
-#     end
-# end
-
 require_relative '../validator'
 
 describe 'validates' do
