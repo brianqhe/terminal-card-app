0cc574818b69847446bde29aee102b0e8529be25
brianskiies <brian.he3@gmail.com>
2020-12-14 09:16:07 +1100

    tidied up artii and added level indicators

--- instructions.rb
+++ instructions.rb
@@ -9,24 +9,20 @@ def instructions()
     card4 = [["┌───────────┐"],["│ Q         │"],["│           │"],["│           │"],["│     #{"♥".colorize(:red)}     │"],["│           │"],["│           │"],["│         Q │"],["└───────────┘"],]
     puts "This game comprises of 4 levels"
     puts "You can only progress to the next level if you guess right, otherwise you start again!"
-    a = Artii::Base.new 
     puts a.asciify("Level 1").colorize(:light_cyan)
     puts "Guess whether the card revealed will be black or red"
     puts facedown_card
     puts "\n"
     puts card1
-    a = Artii::Base.new 
     puts a.asciify("Level 2").colorize(:light_cyan)
     puts "Guess whether the next card revealed will be higher or lower your first card"
     puts "NOTE: If it's the same you automatically lose... (Goalpost rule)"
     puts card2
-    a = Artii::Base.new 
     puts a.asciify("Level 3").colorize(:light_cyan)
     puts "Guess whether the next card is inside or outside of the range of the previous two card"
     puts card3
     puts "In the example above, 5 is inside the range of 4 and 9, therefore a correct guess of 'inside' will make you correct"
     puts "NOTE: If the card is the same as any of your previous two cards, you automatically lose! (Goalpost rule)"
-    a = Artii::Base.new 
     puts a.asciify("Level 4").colorize(:light_cyan)
     puts card4
     puts "Guess the suit of your last card"
