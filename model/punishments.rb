5a262346dd77cf62b42e8c319803e4a4cb3d3350
brianskiies <brian.he3@gmail.com>
2020-12-14 15:35:46 +1100

    updated punishments display

--- model/punishments.rb
+++ model/punishments.rb
@@ -3,10 +3,11 @@ module Punishments
     def display_punishments(array)
         puts "\nThe current list of punishments are -\n"
         array.each {|x| puts "\t #{x.capitalize}"}
+        puts "\n"
     end
 
     def random_punshiment(array)
-        array.sample
+        "The punishment they have to perform is #{array.sample}"
     end
 
     def selected_punishment(array)
